4aa25bb27e47b11dd6cb5965ea84327ff556d074
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-26 20:51:21 +0530

    Changes in editor view
    Added CTRL + S event
    added 2 new button UI
    navigations

--- src/pages/EditorView.jsx
+++ src/pages/EditorView.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/EditorView.css";
+import { useNavigate } from "react-router-dom";
 function EditorView() {
   const [text, setText] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.ctrlKey && event.key === "s") {
+        event.preventDefault();
+        alert("Clicked Shi");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  const handleHomeBtn = () => {
+    navigate("/");
+  };
 
   return (
     <div className="app">
       <div className="sidebar">
-        <button className="sidebar-btn">+ New Page</button>
+        <div className="sidebar-btn-small-container">
+          <button
+            className="sidebar-btn-small btn-hover"
+            onClick={() => handleHomeBtn()}
+          >
+            Home
+          </button>
+          <button className="sidebar-btn-small btn-hover">Minimize</button>
+        </div>
+        <button className="sidebar-btn btn-hover">+ New Page</button>
       </div>
 
       <div className="main">
