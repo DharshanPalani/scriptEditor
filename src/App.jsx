b94b9c397a141576964957877c551b1bd62d8b6f
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-26 20:21:33 +0530

    Added menu and few navigation for UI testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import EditorView from "./pages/EditorView";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import EditorView from "./pages/EditorView.jsx";
+import Menu from "./pages/Menu.jsx";
 
 function App() {
-  return <EditorView />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/editor" element={<EditorView />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/Menu.css";
+import { useNavigate } from "react-router-dom";
+
+function Menu() {
+  const navigate = useNavigate();
+
+  const handleNewProject = () => {
+    navigate("/editor");
+  };
+
+  return (
+    <div className="menu-app">
+      <h1 className="menu-title">Manga Editor</h1>
+      <button className="menu-btn" onClick={() => handleNewProject()}>
+        New Project
+      </button>
+      <button className="menu-btn">Open Project</button>
+    </div>
+  );
+}
+
+export default Menu;
