6d6bfacee8b6e0994da7d87f53f5ff86b294f143
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-27 00:34:58 +0530

    Added project view page and styled
    also added few func like project select and double click to navigate to
    editor
    
    for now all of these are only UI with no func testing only with dummy
    datas for project view
    
    soon should add fs and have 2 options
    
    to either store data in sqlite for 50-60% like project directory, few
    content etc and depend only on .md files for writing scripts or fully
    depend on fs system which reads each and every folder as project
    
    or use both (:

--- src/pages/ProjectView.jsx
+++ src/pages/ProjectView.jsx
@@ -1,24 +1,42 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "../css/ProjectView.css";
 
 function ProjectView() {
   const { id } = useParams();
+  const [projects] = useState(["Project A", "Project B", "Project C"]);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-  const [text, setText] = useState("");
+  const navigation = useNavigate();
 
-  useEffect(() => {
-    if (id === "1234") {
-      setText("Existing project");
-    } else {
-      setText("This project ain't there");
-    }
-  }, []);
+  const handleDoubleClick = (project) => {
+    // console.log("Double clicked:", project);
+    navigation("/editor");
+  };
 
   return (
-    <>
-      <div style={{ color: "red" }}>{id} Daw</div>
-      <h1 style={{ color: "red" }}>{text}</h1>
-    </>
+    <div className="project-app">
+      <div className="project-sidebar">
+        <h3>Projects</h3>
+        {projects.map((project, index) => (
+          <button
+            key={index}
+            className={`project-sidebar-btn btn-hover ${
+              selectedProject === project ? "selected" : ""
+            }`}
+            onClick={() => setSelectedProject(project)}
+            onDoubleClick={() => handleDoubleClick(project)}
+          >
+            {project}
+          </button>
+        ))}
+      </div>
+
+      <div className="project-main">
+        <div className="top-pane">Top daw</div>
+        <div className="bottom-pane">Bottom daw</div>
+      </div>
+    </div>
   );
 }
 
