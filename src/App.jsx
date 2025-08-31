cfcee28e698f56667e6b84eaa0894266a5554579
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-31 23:16:21 +0530

    Added escape func changes idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
 import EditorView from "./pages/EditorView.jsx";
 import Menu from "./pages/Menu.jsx";
 import ProjectView from "./pages/ProjectView.jsx";
@@ -10,19 +10,33 @@ import OpenProjectView from "./pages/OpenProjectView.jsx";
 function App() {
   const { initApplication } = useInitApplication();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     initApplication();
   }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        navigate(-1);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path={"/project/:id"} element={<ProjectView />} />
-        <Route path="/editor" element={<EditorView />} />
-        <Route path="/new-project" element={<NewProjectView />} />
-        <Route path="/open-project" element={<OpenProjectView />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Menu />} />
+      <Route path={"/project/:id"} element={<ProjectView />} />
+      <Route path="/editor" element={<EditorView />} />
+      <Route path="/new-project" element={<NewProjectView />} />
+      <Route path="/open-project" element={<OpenProjectView />} />
+    </Routes>
   );
 }
 
